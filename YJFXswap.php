14cfa9a0dd31cea7bad274a0bfee2d741302451a
24takumi <n24tkm@gmail.com>
2019-04-03 23:58:51 +0900

    Update YJFXswap.php
    次月分のスワップポイントのデータが新しいフィールドが作成されないうえに、前月分のデータを上書きしてしまうバグを修正。

--- YJFXswap.php
+++ YJFXswap.php
@@ -90,17 +90,23 @@
   $stmt=$pdo->query($sql);
 
    ?>
-  <?php //毎月のテーブルを新規で自動作成する
+  <?php
+  //毎月のテーブルを新規で自動作成する
   //今月の最後の日を求める
   $d = new DateTime();
   $d->setDate($d->format('Y'), $d->format('m'), 1);
   $d->modify('+1 month -1 day');
-  $end_date=$d->format('d');
+  $end_day=$d->format('d');//今月の最後を返す
   $current_month=$d->format('m');
+  $monthmax = intval($pdo->query("SELECT max(month) FROM YJFX")->fetchColumn());
+  $daymax = intval($pdo->query("SELECT max(day) FROM YJFX where month='" . $monthmax . "' ")->fetchColumn());
+
   $current_month_nonzero=$d->format('n');
-  $res = intval($pdo->query("SELECT max(day) FROM YJFX where '" . $current_month . "' ")->fetchColumn());
-  if($res<$end_date){
-    for ($i=1; $i <=$end_date ; $i++) {
+
+  $day_max_i= intval($pdo->query("SELECT max(day) FROM YJFX where month='" . $current_month . "' ")->fetchColumn());
+
+  if($monthmax<$current_month_nonzero){
+    for ($i=1; $i <=$end_day ; $i++) {//今月の最後まで日付などを振る
       $sql=$pdo->prepare("INSERT INTO YJFX (id,created_at,updated_at,year,month,day) VALUES(:id,:created_at,:updated_at,:year,:month,:day)");
       $sql->bindParam(':id',$id,PDO::PARAM_STR);
       $sql->bindParam(':created_at',$created_at,PDO::PARAM_STR);
@@ -116,7 +122,7 @@
 
       $year=$d->format('Y');
       $month=$d->format('m');
-      $day_max = intval($pdo->query("SELECT max(day) FROM YJFX where '" . $current_month . "' ")->fetchColumn());
+      $day_max = intval($pdo->query("SELECT max(day) FROM YJFX where month='" . $current_month . "' ")->fetchColumn());
       $day=$day_max+1;
 
       $sql->execute();
@@ -126,10 +132,12 @@
 
   <?php //今日の日付
   $current_date=date("d");
+
    ?>
 
   <?php
   require_once("./phpQuery-onefile.php");
+
   $url = "https://www.yjfx.jp/gaikaex/mark/swap/calendar.php";
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
@@ -158,6 +166,7 @@
   for ($i=1; $i <$cnt1 ; $i++) {//$iを1から始めることで最初の空白の配列をループしない
     $split2=explode($current_month_nonzero."月",$split1[$i]);
     $cnt2=count($split2);
+
     for ($j=1; $j <$cnt2 ; $j++) {//$jを1から始めることで列名の配列をループしない
       $split3=explode(' ',$split2[$j]);
 
@@ -238,37 +247,41 @@
       }
 
       if($split3[0]<=$current_date){
-        if($i>=1 && $i<=5){
-          $updated_at=date("Y/m/d H:i:s");
-          $days=$split3[0];
-          $sp1s=$split3[1];
-          $buy1s=$split3[2];
-          $sell1s=$split3[3];
-          $sp2s=$split3[4];
-          $buy2s=$split3[5];
-          $sell2s=$split3[6];
-          $sp3s=$split3[7];
-          $buy3s=$split3[8];
-          $sell3s=$split3[9];
-          $sp4s=$split3[10];
-          $buy4s=$split3[11];
-          $sell4s=$split3[12];
-          $sql="UPDATE YJFX set updated_at='$updated_at',{$sp1}='$sp1s',{$buy1}='$buy1s',{$sell1}='$sell1s',{$sp2}='$sp2s',{$buy2}='$buy2s',{$sell2}='$sell2s',{$sp3}='$sp3s',{$buy3}='$buy3s',{$sell3}='$sell3s',{$sp4}='$sp4s',{$buy4}='$buy4s',{$sell4}='$sell4s' WHERE day='$days'";
-          $stmt=$pdo->prepare($sql);
-          $stmt->execute();
-        }
-        if($i==6){
-          $updated_at=date("Y/m/d H:i:s");
-          $days=$split3[0];
-          $sp1s=$split3[1];
-          $buy1s=$split3[2];
-          $sell1s=$split3[3];
-          $sp2s=$split3[4];
-          $buy2s=$split3[5];
-          $sell2s=$split3[6];
-          $sql="UPDATE YJFX set updated_at='$updated_at',{$sp1}='$sp1s',{$buy1}='$buy1s',{$sell1}='$sell1s',{$sp2}='$sp2s',{$buy2}='$buy2s',{$sell2}='$sell2s' WHERE day='$days'";
-          $stmt=$pdo->prepare($sql);
-          $stmt->execute();
+        $p_day=$split3[0];
+        $flag = intval($pdo->query("SELECT day FROM YJFX WHERE month='" . $current_month . "' AND day='" . $p_day . "' ")->fetchColumn());
+        if($flag==""){
+          if($i>=1 && $i<=5){
+            $updated_at=date("Y/m/d H:i:s");
+            $days=$split3[0];
+            $sp1s=$split3[1];
+            $buy1s=$split3[2];
+            $sell1s=$split3[3];
+            $sp2s=$split3[4];
+            $buy2s=$split3[5];
+            $sell2s=$split3[6];
+            $sp3s=$split3[7];
+            $buy3s=$split3[8];
+            $sell3s=$split3[9];
+            $sp4s=$split3[10];
+            $buy4s=$split3[11];
+            $sell4s=$split3[12];
+            $sql="UPDATE YJFX set updated_at='$updated_at',{$sp1}='$sp1s',{$buy1}='$buy1s',{$sell1}='$sell1s',{$sp2}='$sp2s',{$buy2}='$buy2s',{$sell2}='$sell2s',{$sp3}='$sp3s',{$buy3}='$buy3s',{$sell3}='$sell3s',{$sp4}='$sp4s',{$buy4}='$buy4s',{$sell4}='$sell4s' WHERE day='$days' and month='$current_month'";
+            $stmt=$pdo->prepare($sql);
+            $stmt->execute();
+          }
+          if($i==6){
+            $updated_at=date("Y/m/d H:i:s");
+            $days=$split3[0];
+            $sp1s=$split3[1];
+            $buy1s=$split3[2];
+            $sell1s=$split3[3];
+            $sp2s=$split3[4];
+            $buy2s=$split3[5];
+            $sell2s=$split3[6];
+            $sql="UPDATE YJFX set updated_at='$updated_at',{$sp1}='$sp1s',{$buy1}='$buy1s',{$sell1}='$sell1s',{$sp2}='$sp2s',{$buy2}='$buy2s',{$sell2}='$sell2s' WHERE day='$days' and month='$current_month'";
+            $stmt=$pdo->prepare($sql);
+            $stmt->execute();
+          }
         }
       }
     }
